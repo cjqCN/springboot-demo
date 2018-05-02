50b052f82d87c016ea14bc08589c5c65ef33ce38
chenjinquan <chenjinquan@banggood.com>
2018-05-02 14:48:55 +0800

    修改细节

--- src/main/java/com/example/demo/util/TimeUtil.java
+++ src/main/java/com/example/demo/util/TimeUtil.java
@@ -2,13 +2,10 @@ package com.example.demo.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
+
 
 
 /**
@@ -81,50 +78,12 @@ public class TimeUtil {
 }
 
 class SystemClock {
-    private final long period;
-    private final AtomicLong now;
-
-    private SystemClock(long period) {
-        this.period = period;
-        this.now = new AtomicLong(System.currentTimeMillis());
-        scheduleClockUpdating();
-    }
-
-    private static class InstanceHolder {
-        public static final SystemClock INSTANCE = new SystemClock(1);
-    }
-
-    private static SystemClock instance() {
-        return InstanceHolder.INSTANCE;
-    }
-
-    private void scheduleClockUpdating() {
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
-            @Override
-            public Thread newThread(Runnable runnable) {
-                Thread thread = new Thread(runnable, "System Clock");
-                thread.setDaemon(true);
-                return thread;
-            }
-        });
-        scheduler.scheduleAtFixedRate(new Runnable() {
-            public void run() {
-                now.set(System.currentTimeMillis());
-            }
-        }, period, period, TimeUnit.MILLISECONDS);
-    }
-
-    private long currentTimeMillis() {
-        return now.get();
-    }
-
+    static Instant now = Instant.now();
     public static long now() {
-        return instance().currentTimeMillis();
+        return now.toEpochMilli();
     }
-
-
-
 }
+
 
 
 
